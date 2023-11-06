5904e03832289a193fe0e149216e1d33d13b1bc6
harp515 <harp5151017@gmail.com>
2023-11-06 15:59:44 +0900

    :bug:

---
+++ Potarl/Assets/Script/PoolingManger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Script
+{
+    public class PoolingManger : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public int poolSize = 20;
+        
+        public GameObject bullet;
+        public GameObject[] bulletpool;
+        void Start()
+        {
+            bulletpool = new GameObject[poolSize];
+            for (int i = 0; i < poolSize; i++)
+            {
+                GameObject bull = Instantiate(bullet);
+                bull.SetActive(false);
+                bulletpool[i] = bull;
+            }
+        }
+       
+    }
+}
---
+++ Potarl/Assets/Script/Shoot.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace Script
+{
+    public class Shoot : MonoBehaviour
+    {
+        PoolingManger _pool;
+        
+        public Transform firePos;
+        public GameObject player;
+
+        void Awake()
+        {
+            _pool = GetComponent<PoolingManger>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                Fire();
+        }
+        // ReSharper disable Unity.PerformanceAnalysis
+        void Fire()
+        {
+            for (int j = 0; j < _pool.poolSize; j++)
+            {
+                GameObject bullet = _pool.bulletpool[j];
+                if (bullet.activeSelf == false)
+                {
+                    bullet.SetActive(true);
+                    bullet.transform.position = firePos.transform.position;
+                    bullet.transform.rotation = player.transform.rotation;
+                    bullet.GetComponent<Rigidbody>().AddForce(Vector3.forward * (Time.deltaTime * 5));
+                    break;
+                }
+            }
+        }
+    }
+}
